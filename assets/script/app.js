0d248fe2e0a3ce60c3943746822a1843f70b83c4
Harman <harmandeepsingh7@student.mitt.ca>
2025-11-29 22:24:46 -0600

    Added a js code

--- assets/script/app.js
+++ assets/script/app.js
@@ -29,9 +29,6 @@ function getCookie(name) {
     return null;
 }
 
-/* --------------------------------------------------
-   BROWSER + OS DETECTION
--------------------------------------------------- */
 
 function getBrowser() {
     const ua = navigator.userAgent;
@@ -55,3 +52,77 @@ function getOS() {
 
     return "Unknown OS";
 }
+
+
+const cookieModal = select("#cookieModal");
+const settingsModal = select("#settingsModal");
+
+const acceptAllBtn = select("#acceptAllBtn");
+const settingsBtn = select("#settingsBtn");
+const saveSettingsBtn = select("#saveSettingsBtn");
+
+const browserToggle = select("#browserToggle");
+const osToggle = select("#osToggle");
+const screenToggle = select("#screenToggle");
+
+const COOKIE_LIFETIME = 17;
+
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    const hasCookie =
+        getCookie("browser") ||
+        getCookie("os") ||
+        getCookie("screen") ||
+        getCookie("reject");
+
+    if (!hasCookie) {
+        setTimeout(() => cookieModal.showModal(), 600);
+    }
+});
+
+
+listen("click", acceptAllBtn, () => {
+
+    setCookie("browser", getBrowser(), COOKIE_LIFETIME);
+    setCookie("os", getOS(), COOKIE_LIFETIME);
+
+    const screenData = `${screen.width}x${screen.height}`;
+    setCookie("screen", screenData, COOKIE_LIFETIME);
+
+    cookieModal.close();
+});
+
+
+listen("click", settingsBtn, () => {
+    cookieModal.close();
+    settingsModal.showModal();
+});
+
+
+listen("click", saveSettingsBtn, () => {
+
+    let createdCookie = false;
+
+    if (browserToggle.checked) {
+        setCookie("browser", getBrowser(), COOKIE_LIFETIME);
+        createdCookie = true;
+    }
+
+    if (osToggle.checked) {
+        setCookie("os", getOS(), COOKIE_LIFETIME);
+        createdCookie = true;
+    }
+
+    if (screenToggle.checked) {
+        const screenData = `${screen.width}x${screen.height}`;
+        setCookie("screen", screenData, COOKIE_LIFETIME);
+        createdCookie = true;
+    }
+
+    if (!createdCookie) {
+        setCookie("reject", "true", COOKIE_LIFETIME);
+    }
+
+    settingsModal.close();
+});
