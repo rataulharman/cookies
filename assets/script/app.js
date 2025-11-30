062f66bf40728ea5b71a7cac98a8679e881c127a
Harman <harmandeepsingh7@student.mitt.ca>
2025-11-29 22:07:34 -0600

    getcookie function

--- assets/script/app.js
+++ assets/script/app.js
@@ -14,3 +14,17 @@ function setCookie(name, value, maxAge) {
 
     document.cookie = cookieString;
 }
+
+function getCookie(name) {
+    const cookies = document.cookie.split(";");
+
+    for (let cookie of cookies) {
+        const [cookieName, cookieValue] = cookie.split("=").map(c => c.trim());
+
+        if (decodeURIComponent(cookieName) === name) {
+            return decodeURIComponent(cookieValue);
+        }
+    }
+
+    return null;
+}
